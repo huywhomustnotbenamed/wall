bcd75d4fe52124cbf82fd2b7341355569587d4f5
huyhuynh408 <huyhuynh85@gmail.com>
2016-04-15 12:47:20 -0500

    set up basic ng app

--- ngApp.js
+++ ngApp.js
@@ -1,7 +1,11 @@
 var app = angular.module('before', []);
 
 app.controller('mainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
+  '$scope',
+  function($scope){
+    $scope.responses = [
+      {content: 'Pilot and airplane', date: '1/1/2016'},
+      {content: 'Live in Spain', date: '1/1/2016'},
+      {content: 'See the Grand Canyon', date: '1/1/2016'}
+    ];
 }]);
