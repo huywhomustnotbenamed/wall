ec0a0aab2a1ab2f75d9955b19ea53337058efacc
huyhuynh408 <huyhuynh85@gmail.com>
2016-05-06 16:14:00 -0400

    added home state

--- ngApp.js
+++ ngApp.js
@@ -1,5 +1,19 @@
 var app = angular.module('never', ['ui.router']);
 
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'mainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
 app.controller('mainCtrl', [
   '$scope',
   'responses',
